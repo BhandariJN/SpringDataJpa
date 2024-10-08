a3bd6b5fdcc3fd513d9185b94398026d0b45d805
meetme.Jn <royaldukhee@gmail.com>
2024-10-08 07:18:55 +0545

    Defining Table Properties via column annotations

--- src/main/java/com/jn/springdatajpa/model/Authors.java
+++ src/main/java/com/jn/springdatajpa/model/Authors.java
@@ -1,10 +1,10 @@
 package com.jn.springdatajpa.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -12,10 +12,37 @@ import lombok.NoArgsConstructor;
 public class Authors {
     @Id
     @GeneratedValue
+           /* (
+            strategy = GenerationType.SEQUENCE,
+            generator = "author_sequence"
+    )
+    @SequenceGenerator(
+
+            name = "author_sequence",
+            sequenceName = "author_sequence",
+            allocationSize = 1
+    )
+    @TableGenerator(name = "author_id_gen",
+    table = "id_generation",
+    pkColumnName = "id_name",
+    valueColumnName = "id_value",
+    allocationSize = 1)*/
+
     private Integer id;
+
+    @Column(name = "f_name",
+    length = 50,
+    nullable = false)
     private String firstName;
     private String lastName;
+    @Column(unique = true,
+    nullable = false)
     private String email;
     private int age;
 
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(insertable = false)
+    private LocalDateTime modifiedAt;
 }
